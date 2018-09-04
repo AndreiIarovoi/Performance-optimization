b48de2a5880387ea239aad9b9e68e9b4eb7ee223
Andrei Iarovoi <iarovoi.andrei@gmail.com>
2018-09-04 18:55:28 +0300

    RespondentsService.GetRespondents string.IsNullOrEmpty(filter.ExperienceLevel). Why check it in db if you can check it in app.

--- StackOverflowSurvey.Service/RespondentsService.cs
+++ StackOverflowSurvey.Service/RespondentsService.cs
@@ -22,18 +22,26 @@ namespace StackOverflowSurvey.Service
 
         private readonly IExperienceLevelCache experienceLevelCache;
 
+        private readonly ICompanySizeRepository companySizeRepository;
+
+        private readonly IExperienceLevelRepository experienceLevelRepository;
+
         public RespondentsService(
             IRespondentsReader respondentsReader,
             IRespondentRepository respondentRepository,
             IRespondentsValidator respondentValidator,
             ICompanySizeCache companySizeCache,
-            IExperienceLevelCache experienceLevelCache)
+            IExperienceLevelCache experienceLevelCache,
+            ICompanySizeRepository companySizeRepository,
+            IExperienceLevelRepository experienceLevelRepository)
         {
             this.respondentsReader = respondentsReader;
             this.respondentRepository = respondentRepository;
             this.respondentValidator = respondentValidator;
             this.companySizeCache = companySizeCache;
             this.experienceLevelCache = experienceLevelCache;
+            this.companySizeRepository = companySizeRepository;
+            this.experienceLevelRepository = experienceLevelRepository;
         }
 
         public IEnumerable<RespondentsCreationResult> CreateRespondents(IEnumerable<PostedFile> postedFiles)
@@ -49,9 +57,54 @@ namespace StackOverflowSurvey.Service
 
         public IEnumerable<Respondent> GetRespondents(RespondentsFilter filter)
         {
-            return respondentRepository.GetFiltered(CreateRespondentsQuery(filter)).Select(this.MapRespondent)
-                .Where(r => (string.IsNullOrEmpty(filter.ExperienceLevel) || r.ExperienceLevel.Contains(filter.ExperienceLevel))
-                        && (string.IsNullOrEmpty(filter.CompanySize) || r.CompanySize.Contains(filter.CompanySize))).AsEnumerable();
+            IQueryable<RespondentInfo> respInfo = this.respondentRepository.GetFiltered(this.CreateRespondentsQuery(filter));
+
+            if (!string.IsNullOrEmpty(filter.CompanySize))
+            {
+                respInfo = from res in respInfo
+                            join cmp in this.companySizeRepository.GetAll()
+                            on res.CompanySize equals cmp.Size
+                            where cmp.Class == filter.CompanySize
+                           select res;
+            }
+
+            if (!string.IsNullOrEmpty(filter.ExperienceLevel))
+            {
+                respInfo = from res in respInfo
+                    join exp in this.experienceLevelRepository.GetAll()
+                        on res.ExperienceLevel equals exp.YearsProgram
+                    where exp.Level == filter.ExperienceLevel
+                    select res;
+            }
+
+            IEnumerable<Respondent> respondents = 
+                respInfo
+                .Select(respondent => new Respondent
+                {
+                    Id = respondent.Id,
+                    Country = respondent.Country,
+                    DeveloperType = respondent.DeveloperType,
+                    Gender = respondent.Gender,
+                    Professional = respondent.Professional,
+                    CompanySize = respondent.CompanySize,
+                    Language = respondent.Language,
+                    VersionControl = respondent.VersionControl,
+                    WorkStart = respondent.WorkStart,
+                    ExperienceLevel = respondent.ExperienceLevel,
+                    CareerSatisfaction = respondent.CareerSatisfaction,
+                    JobSatisfaction = respondent.JobSatisfaction
+                });
+
+
+            respondents = respondents.ToList();
+
+            foreach (Respondent respondent in respondents)
+            {
+                respondent.CompanySize = this.companySizeCache.GetCompanyClass(respondent.CompanySize);
+                respondent.ExperienceLevel = this.experienceLevelCache.GetExperienceLevel(respondent.ExperienceLevel);
+            }
+
+            return respondents;
         }
 
         private RespondentsQuery CreateRespondentsQuery(RespondentsFilter filter)
@@ -67,7 +120,7 @@ namespace StackOverflowSurvey.Service
             };
         }
 
-        private Respondent MapRespondent(RespondentInfo respondent)
+        /*private Respondent MapRespondent(RespondentInfo respondent)
         {
             Respondent newRespondent = new Respondent
             {
@@ -76,16 +129,16 @@ namespace StackOverflowSurvey.Service
                 DeveloperType = respondent.DeveloperType,
                 Gender = respondent.Gender,
                 Professional = respondent.Professional,
-                CompanySize = this.companySizeCache.GetCompanySizesCache(respondent.CompanySize),
+                CompanySize = this.companySizeCache.GetCompanyClass(respondent.CompanySize),
                 Language = respondent.Language,
                 VersionControl = respondent.VersionControl,
                 WorkStart = respondent.WorkStart,
-                ExperienceLevel = this.experienceLevelCache.GetExperienceLevelCache(respondent.ExperienceLevel),
+                ExperienceLevel = this.experienceLevelCache.GetExperienceLevel(respondent.ExperienceLevel),
                 CareerSatisfaction = respondent.CareerSatisfaction,
                 JobSatisfaction = respondent.JobSatisfaction
              };
 
             return newRespondent;
-        }
+        }*/
     }
 }
